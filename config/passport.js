8b6adebff08efdfee70ec271e4d47e19b54f84dd
Nagashree V <nagashreevkrishna@gmail.com>
2026-05-18 18:44:08 +0530

    fix passport syntax

--- config/passport.js
+++ config/passport.js
@@ -1,48 +1,66 @@
-const passport       = require('passport');
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User           = require('../models/User');
+const User = require('../models/User');
 
-passport.use(new GoogleStrategy(
-  {
-    clientID:     process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:  `${process.env.SERVER_URL || 'https://devhire-jdmi.onrender.com'}/api/auth/google/callback`
-  },
-async (_accessToken, _refreshToken, profile, done) => {
-  try {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `${process.env.SERVER_URL || 'https://devhire-jdmi.onrender.com'}/api/auth/google/callback`
+    },
 
-    let user = await User.findOne({
-      $or: [
-        { googleId: profile.id },
-        { email: profile.emails?.[0]?.value }
-      ]
-    });
+    async (_accessToken, _refreshToken, profile, done) => {
+      try {
 
-    if (!user) {
+        let user = await User.findOne({
+          $or: [
+            { googleId: profile.id },
+            { email: profile.emails?.[0]?.value }
+          ]
+        });
 
-      user = await User.create({
-        googleId: profile.id,
-        name: profile.displayName,
-        email: profile.emails?.[0]?.value || '',
-        avatar: profile.photos?.[0]?.value || ''
-      });
+        if (!user) {
 
-    } else {
+          user = await User.create({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails?.[0]?.value || '',
+            avatar: profile.photos?.[0]?.value || ''
+          });
 
-      // Attach googleId to existing account if missing
-      if (!user.googleId) {
-        user.googleId = profile.id;
+        } else {
+
+          // Attach googleId if missing
+          if (!user.googleId) {
+            user.googleId = profile.id;
+          }
+
+          // Update avatar
+          user.avatar = profile.photos?.[0]?.value || user.avatar;
+
+          await user.save();
+        }
+
+        return done(null, user);
+
+      } catch (err) {
+        return done(err, null);
       }
-
-      // Update avatar
-      user.avatar = profile.photos?.[0]?.value || user.avatar;
-
-      await user.save();
     }
+  )
+);
 
-    return done(null, user);
+// Store only user id in session
+passport.serializeUser((user, done) => done(null, user._id));
 
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
   } catch (err) {
-    return done(err, null);
+    done(err, null);
   }
-}
+});
+
+module.exports = passport;
